a75022a5801c2d51fa0a05ebeb531f6a49aed51d
hiroMTB <maobahiroshi@gmail.com>
2017-07-20 15:56:36 +0200

    add GL3 window setting

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,11 +1,15 @@
 #include "ofApp.h"
 
 int main(){
+	
+	ofGLWindowSettings settings;
+	settings.setGLVersion(3,2);
+	settings.width = 1024;
+	settings.height = 1024;
+	ofCreateWindow(settings);
+    
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
-
-	ofSetupOpenGL(1024, 1024, OF_WINDOW);
-
 	ofRunApp(new ofApp());
 }
